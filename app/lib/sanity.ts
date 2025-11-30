5544ecdfe61dabc9478748827a4fc3dd7b70829c
CraftsAndKits <craftsandkits.com@gmail.com>
2025-11-30 23:05:40 +0100

    Fixed sanity image builder and dependencies

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,16 @@
 import { createClient } from "next-sanity";
+import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: "nrmr5169", // <--- DELETE THIS TEXT AND PASTE YOUR ID
+  projectId: "nrmr5169", // Double check this is your ID!
   dataset: "production",
   apiVersion: "2024-01-01",
-  useCdn: false, // We use false so you see updates instantly
+  useCdn: false,
 });
+
+// This is the missing helper function!
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
