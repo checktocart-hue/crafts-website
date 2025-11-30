0188f47d0fc100ef13161d0c7dccec3d71e90c4d
CraftsAndKits <craftsandkits.com@gmail.com>
2025-11-30 23:14:12 +0100

    Fix sanity image import name

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,15 +1,15 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from '@sanity/image-url'
+import createImageUrlBuilder from '@sanity/image-url'; // <--- Renamed this based on the error
 
 export const client = createClient({
-  projectId: "nrmr5169", // Double check this is your ID!
+  projectId: "nrmr5169", 
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
 });
 
-// This is the missing helper function!
-const builder = imageUrlBuilder(client);
+// Use the new name here
+const builder = createImageUrlBuilder(client);
 
 export function urlFor(source: any) {
   return builder.image(source);
