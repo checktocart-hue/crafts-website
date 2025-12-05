b9bfcba68f8f51373bebf969c778e04c74b3c55e
CraftsAndKits <craftsandkits.com@gmail.com>
2025-12-05 14:31:19 +0100

    Moved category filtering to main reviews page

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,9 +1,5 @@
 import { createClient } from "next-sanity";
-import { createImageUrlBuilder } from "next-sanity/image"; 
-// Note: next-sanity often includes the image builder now, 
-// but let's stick to the package you have installed and fix the import style:
-
-import { createImageUrlBuilder } from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "nrmr5169",
@@ -12,7 +8,7 @@ export const client = createClient({
   useCdn: true,
 });
 
-const builder = createImageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
 export function urlFor(source: any) {
   return builder.image(source);
