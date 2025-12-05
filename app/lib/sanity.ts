00410e43df40fb8fc6b17f3d3ed5ba14f5488291
CraftsAndKits <craftsandkits.com@gmail.com>
2025-12-05 13:53:11 +0100

    Fixed sanity image builder import

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,5 +1,9 @@
 import { createClient } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
+import { createImageUrlBuilder } from "next-sanity/image"; 
+// Note: next-sanity often includes the image builder now, 
+// but let's stick to the package you have installed and fix the import style:
+
+import { createImageUrlBuilder } from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "nrmr5169",
@@ -8,8 +12,7 @@ export const client = createClient({
   useCdn: true,
 });
 
-// --- THIS WAS MISSING ---
-const builder = imageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 
 export function urlFor(source: any) {
   return builder.image(source);
