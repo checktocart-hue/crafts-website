d0e1a02fdf97d3c7f87486c803376b42a8b40077
CraftsAndKits <craftsandkits.com@gmail.com>
2025-12-05 13:24:07 +0100

    Added urlFor helper to sanity lib

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,16 @@
 import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
   projectId: "nrmr5169",
   dataset: "production",
   apiVersion: "2024-01-01",
-  useCdn: false,
+  useCdn: true,
 });
+
+// --- THIS WAS MISSING ---
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: any) {
+  return builder.image(source);
+}
